542126774a5aebe2aed830686665921acff30345
SquirrelSong5 <1825989650@qq.com>
2025-04-18 16:36:29 +0800

    静态资源创建

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//引入初始化样式文件
+import '@/styles/common.scss'
+
 
 //测试接口函数
 import  { testAPI }  from '@/apis/testAPI.js'
@@ -16,6 +19,7 @@ testAPI().then(res => {
   console.error(err)
 })
 const app = createApp(App)
+
 
 app.use(createPinia())
 app.use(router)
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,8 +1,6 @@
 <template>
     <div>
         我是home页面
-        <!-- 二级路由出口 -->
-        <router-view />
     </div>
 </template>
 <script>
