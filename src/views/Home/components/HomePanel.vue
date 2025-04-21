f7a791d9529555b668b441c64a0bfe2fa192d061
SquirrelSong5 <1825989650@qq.com>
2025-04-21 21:18:41 +0800

    home面板组件封装(props+slot)

---
+++ src/views/Home/components/HomePanel.vue
@@ -0,0 +1,59 @@
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        default: '新鲜好物'
+    },
+    subtitle: {
+        type: String,
+        default: '新鲜出炉 品质靠谱'
+    }
+})
+
+
+</script>
+
+
+<template>
+    <div class="home-panel">
+        <div class="container">
+            <div class="head">
+                <!-- 主标题和副标题 -->
+                <h3>
+                    {{ title }}<small>{{ subtitle }}</small>
+                </h3>
+            </div>
+            <div>
+                <!-- 内容区域 -->
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang='scss'>
+.home-panel {
+    background-color: #fff;
+
+    .head {
+        padding: 40px 0;
+        display: flex;
+        align-items: flex-end;
+
+        h3 {
+            flex: 1;
+            font-size: 32px;
+            font-weight: normal;
+            margin-left: 6px;
+            height: 35px;
+            line-height: 35px;
+
+            small {
+                font-size: 16px;
+                color: #999;
+                margin-left: 20px;
+            }
+        }
+    }
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 <template>
     <div>
@@ -14,7 +15,17 @@ import homeProduct from './components/HomeProduct.vue'
         <div>
             <HomeNew />
             <HomeHot />
-            <homeProduct />
+            <HomeProduct />
+            <HomePanel :title="title" :subtitle="subtitle">
+                <div>
+                    home panel content
+                </div>
+            </HomePanel>
+            <HomePanel :title="title" :subtitle="subtitle">
+                <div>
+                    home panel content
+                </div>
+            </HomePanel>
         </div>
     </div>
 </template>
