1f83fd199dd401021e7ad54fdc375c97d2611dd7
SquirrelSong5 <1825989650@qq.com>
2025-04-21 22:19:04 +0800

    home-GoodsItem组件封装

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -1,7 +1,8 @@
 <script setup>
 import HomePanel from './HomePanel.vue'
-import { getGoodsAPI } from '@/apis/home';
-import { ref, onMounted } from 'vue';
+import { getGoodsAPI } from '@/apis/home'
+import { ref, onMounted } from 'vue'
+import GoodsItem from './GoodsItem.vue'
 
 const goodsProduct = ref([]);
 const getGoods = async () => {
@@ -18,20 +19,15 @@ onMounted(() => {
         <HomePanel :title="cate.name" v-for="cate in goodsProduct" :key="cate.id">
             <div class="box">
                 <RouterLink class="cover" to="/">
-                    <img v-img-lazy="cate.picture" />
+                    <img v-img-lazy-load="cate.picture" />
                     <strong class="label">
                         <span>{{ cate.name }}馆</span>
                         <span>{{ cate.saleInfo }}</span>
                     </strong>
                 </RouterLink>
                 <ul class="goods-list">
-                    <li v-for="good in cate.goods" :key="good.id">
-                        <RouterLink to="/" class="goods-item">
-                            <img v-img-lazy="goods.picture" alt="" />
-                            <p class="name ellipsis">{{ good.name }}</p>
-                            <p class="desc ellipsis">{{ good.desc }}</p>
-                            <p class="price">&yen;{{ good.price }}</p>
-                        </RouterLink>
+                    <li v-for="goods in cate.goods" :key="goods.id">
+                        <GoodsItem :goods="goods" />
                     </li>
                 </ul>
             </div>
@@ -127,41 +123,7 @@ onMounted(() => {
             }
         }
 
-        .goods-item {
-            display: block;
-            width: 220px;
-            padding: 20px 30px;
-            text-align: center;
-            transition: all .5s;
 
-            &:hover {
-                transform: translate3d(0, -3px, 0);
-                box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-            }
-
-            img {
-                width: 160px;
-                height: 160px;
-            }
-
-            p {
-                padding-top: 10px;
-            }
-
-            .name {
-                font-size: 16px;
-            }
-
-            .desc {
-                color: #999;
-                height: 29px;
-            }
-
-            .price {
-                color: $priceColor;
-                font-size: 20px;
-            }
-        }
     }
 }
 </style>
