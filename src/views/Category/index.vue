47ca9ccc8ddd93042ef6a62dd4167158d8a82e57
SquirrelSong5 <1825989650@qq.com>
2025-05-03 15:40:36 +0800

    分类Banner渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,6 +2,7 @@
 import { getTopCategoryAPI } from '@/apis/category'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import { getBannerAPI } from '@/apis/home'
 
 const categoryData = ref({})
 const route = useRoute()
@@ -14,6 +15,18 @@ onMounted(() => {
     getCategory(route.params.id)
     console.log('categoryData.value: ', categoryData);
 })
+
+const bannerList = ref([])
+
+const getBanner = async () => {
+    const res = await getBannerAPI(
+        { distributionSite: '2' }
+    )
+    bannerList.value = res.data.result
+}
+
+onMounted(() => getBanner())
+
 
 
 </script>
@@ -28,12 +41,32 @@ onMounted(() => {
                     <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
                 </el-breadcrumb>
             </div>
+            <!-- 轮播图 -->
+            <div class="home-banner">
+                <el-carousel height="500px">
+                    <el-carousel-item v-for="item in bannerList" :key="item.id">
+                        <img :src="item.imgUrl" alt="">
+                    </el-carousel-item>
+                </el-carousel>
+            </div>
         </div>
     </div>
 </template>
 
 
 <style scoped lang="scss">
+.home-banner {
+    width: 1240px;
+    height: 500px;
+    z-index: 98;
+    margin: 0 auto;
+
+    img {
+        width: 100%;
+        height: 500px;
+    }
+}
+
 .top-category {
     h3 {
         font-size: 28px;
