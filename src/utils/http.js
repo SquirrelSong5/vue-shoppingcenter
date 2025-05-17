170994fd25d8cb720b74d1b30919e2fe70b703da
SquirrelSong5 <1825989650@qq.com>
2025-05-17 17:47:52 +0800

    登录-请求拦截器携带token

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,38 +1,46 @@
-import axios from 'axios'
-import { ElMessage } from 'element-plus'
-import 'element-plus/theme-chalk/el-message.css'
+import axios from "axios";
+import { ElMessage } from "element-plus";
+import "element-plus/theme-chalk/el-message.css";
+import { useUserStore } from "@/stores/user";
 // 创建axios实例
 const httpInstance = axios.create({
-  baseURL: 'https://pcapi-xiaotuxian-front-devtest.itheima.net',
-  timeout: 5000
-})
+  baseURL: "https://pcapi-xiaotuxian-front-devtest.itheima.net",
+  timeout: 5000,
+});
 
 // 添加请求拦截器
 httpInstance.interceptors.request.use(
-  config => {
+  (config) => {
     // 在发送请求之前做些什么
-    return config
+    // 1. 从pinia获取token数据
+    const userStore = useUserStore();
+    // 2. 按照后端的要求拼接token数据
+    const token = userStore.userInfo.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
   },
-  error => {
+  (error) => {
     // 对请求错误做些什么
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
 // 添加响应拦截器
 httpInstance.interceptors.response.use(
-  response => {
+  (response) => {
     // 对响应数据做点什么
-    return response
+    return response;
   },
-  error => {
+  (error) => {
     // 对响应错误做点什么
     ElMessage({
-      type: 'warning',
-      message: error.response.data.message
-    })
-    return Promise.reject(error)
+      type: "warning",
+      message: error.response.data.message,
+    });
+    return Promise.reject(error);
   }
-)
+);
 
-export default httpInstance
+export default httpInstance;
