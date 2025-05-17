d59ec758eebc18484155c9bae4a447587b9a44c4
SquirrelSong5 <1825989650@qq.com>
2025-05-17 17:34:44 +0800

    登录-pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
 //引入初始化样式文件
-import '@/styles/common.scss'
-import { lazyPlugin } from '@/directives'
-import { componentPlugin } from '@/components'
+import "@/styles/common.scss";
+import { lazyPlugin } from "@/directives";
+import { componentPlugin } from "@/components";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
-const app = createApp(App)
-
-
-app.use(createPinia())
-app.use(router)
-app.use(lazyPlugin)
-app.use(componentPlugin)
-app.mount('#app')
-
+const app = createApp(App);
+const pinia = createPinia();
+// 注册持久化插件
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia);
+app.use(router);
+app.use(lazyPlugin);
+app.use(componentPlugin);
+app.mount("#app");
--- src/stores/user.js
+++ src/stores/user.js
@@ -2,16 +2,22 @@ import { loginAPI } from "@/apis/user";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-export const useUserStore = defineStore("user", () => {
-  //定义管理用户数据的state
-  const userInfo = ref({});
-  const getUserInfo = async ({ account, password }) => {
-    const res = await loginAPI({ account, password });
-    userInfo.value = res.data.result;
-  };
+export const useUserStore = defineStore(
+  "user",
+  () => {
+    //定义管理用户数据的state
+    const userInfo = ref({});
+    const getUserInfo = async ({ account, password }) => {
+      const res = await loginAPI({ account, password });
+      userInfo.value = res.data.result;
+    };
 
-  return {
-    userInfo,
-    getUserInfo,
-  };
-});
+    return {
+      userInfo,
+      getUserInfo,
+    };
+  },
+  {
+    persist: true,
+  }
+);
